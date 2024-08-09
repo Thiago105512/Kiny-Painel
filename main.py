6d0ee9e37be0b508baca77550840ef144a2ef36a
Thiago105512 <adv.thiago.monte@gmail.com>
2024-08-09 18:03:05 -0300

    Atualiza main.py e adiciona .gitignore

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from os import execl, path, system
 from sys import executable, argv
 
-if path.exists("aviso_read?") == False:
-    with open('aviso_read?', 'w+') as f:
+# Verifica se o arquivo de aviso já foi lido
+if not path.exists("aviso_read.txt"):
+    with open('aviso_read.txt', 'w+') as f:
         system('cat aviso.txt')
         input()
         f.write('true')
+
+# Tenta importar as bibliotecas necessárias
 try:
-    from requests import get;from TerminalButtons import *
-except:
+    from requests import get
+    from TerminalButtons import *
+except ImportError:
+    # Instala as dependências necessárias se não estiverem presentes
     system('python3 -m pip install --upgrade pip && pip3 install requests TerminalButtons')
     execl(executable, executable, *argv)
+
+# Executa o código principal a partir de um repositório online
 try:
-    exec(
-        get('https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py').text
-    )
+    exec(get('https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py').text)
 except:
     print('Verifique sua conexão com a internet!')
